253221260781b0307ea46b1628976504ac7b6760
Vishnu <vpgit123@gmail.com>
2023-05-27 20:43:23 +1000

    updated user search bar

--- src/components/userSearchBar.tsx
+++ src/components/userSearchBar.tsx
@@ -1,58 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const UserSearchBar: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [results, setResults] = useState<string[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
-  const handleSearch = (event:React.SyntheticEvent) => {
+  const handleSearch = (event: React.SyntheticEvent) => {
     event.preventDefault();
     // Simulating an API call to fetch results
     // Replace this with your actual API call
     setTimeout(() => {
       const fetchedResults = ['Result 1', 'Result 2', 'Result 3'];
       setResults(fetchedResults);
+      setIsOpen(true);
     }, 1000);
   };
 
-  return (
-    <div>
-      <form onSubmit={handleSearch} className="relative w-full flex flex-row" >
-      <div className='rounded-l-md w-full bg-white border border-gray-300 '>
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value.toLowerCase())}
-        // onSubmit={handleSearch}
-        
-        className="w-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        placeholder="Search User"
-      />
-      </div>
-      {results.length > 0 && (
-        <div className="absolute z-10 mt-9 w-64 bg-white border border-gray-300 rounded-md shadow-lg">
-          {results.map((result, index) => (
-            <div key={index} className="px-4 py-2 hover:bg-gray-100">
-              <button onClick={() => {
-                setSearchQuery(result);
-                setResults([]);
-              }}>
-                {result}
-              </button>
+  const handleClickOutside = (event: MouseEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
 
-            </div>
-          ))}
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+  return (
+
+    <form onSubmit={handleSearch} >
+      <label htmlFor="default-search"  className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </div>
-      )}
-      <button
-      type='submit'
-        // onClick={handleSearch}
-        className="w-1/4 px-4 bg-blue-500 text-white font-semibold rounded-r-md"
-      >
-        Search
-      </button>
-      </form>
-    </div>
-  );
+        <input type="search" 
+        value={searchQuery}
+        className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        onChange={(e) => setSearchQuery(e.target.value.toLowerCase())}
+        required 
+        placeholder="Search User"
+        />
+        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+      </div>
+      {results.length > 0 && isOpen && (
+            <div ref={dropdownRef} className=" z-10 w-full bg-white border border-gray-300 rounded-md shadow-lg">
+              {results.map((result, index) => (
+                <div key={index} className="px-4 py-2 hover:bg-gray-100">
+                  <button onClick={() => {
+                    setSearchQuery(result);
+                    setResults([]);
+                  }}>
+                    {result}
+                  </button>
+    
+                </div>
+              ))}
+            </div>
+          )}
+    </form>
+
+  )
 };
 
 export default UserSearchBar;
--- src/pages/admin/completeTask.tsx
+++ src/pages/admin/completeTask.tsx
@@ -5,6 +5,7 @@ import { api } from "~/utils/api";
 import NotLoggedIn from "~/components/universal/NotLoggedIn";
 import UserSearchBar from "~/components/userSearchBar";
 import { useState } from "react";
+
 const CompleteTask: NextPage = () => {
   // display some profile
   const { status, data: session } = useSession();
@@ -13,39 +14,33 @@ const CompleteTask: NextPage = () => {
   // if unauthenticated, redirect to login page
   if (!session) return <NotLoggedIn />;
 
-  const data = api.quests.getUserQuests.useQuery({ id: session.id });
-  const handleTaskCompletion = () => {
-    console.log(data);
-  };
 
 
-
-
-  if (status === "authenticated")
-    return (
-      <Layout>
-        <main className="flex min-h-screen flex-col items-center ">
-          <div className="container  mt-6 flex flex-col justify-center items-center px-4 py-16 ">
-            <h1 className="text-center text-5xl  font-extrabold tracking-tight text-white sm:text-[5rem]">
+  return (
+    <Layout>
+      <main className="flex min-h-screen flex-col items-center ">
+        <div className="container  mt-6 flex flex-col justify-center items-center px-4 py-16 ">
+          <h1 className="text-center text-5xl  font-extrabold tracking-tight text-white sm:text-[5rem]">
             Complete Quests 🔱
-            </h1>
+          </h1>
 
 
-            <div className="flex w-full flex-col h-full justify-evenly py-5">
-              <div>Pick Participant</div>
-              <UserSearchBar/>
+          <div className="flex w-full flex-col h-full justify-evenly py-5">
+            <div>Pick Participant</div>
+            <UserSearchBar />
+            
 
-              <div className="h-20 overflow-scroll"> 
-          
-              
-              </div>
+            <div className="h-20 overflow-scroll">
+
+
             </div>
           </div>
-        </main>
+        </div>
+      </main>
 
 
-      </Layout>
-    );
+    </Layout>
+  );
 
   return <div>loading...</div>;
 };
