dd5f89c0823d00c02473bb726594c3a29350d586
Vishnu <vpgit123@gmail.com>
2023-06-01 11:33:44 +1000

    task card ui update

--- src/components/universal/TaskCard.tsx
+++ src/components/universal/TaskCard.tsx
@@ -2,11 +2,17 @@
 
 const TaskCard = ({
   task,
+  selected
 }: {
-  task: { name: string; points: number; id: string };
+  task: { name: string; points: number; id: string },
+  selected: boolean
 }) => {
   return (
-    <div className="rounded-xl border w-40 h-40 border-white bg-white/10 p-2 text-white backdrop-blur-md">
+    <div className={`w-40 h-40 rounded-xl p-2 
+    ${selected ? 
+      'border-green-500 text-green-500 border-4 shadow-xl bg-green-900/20' 
+    : 'text-white border-gray-300 border'}
+    `}>
       <h1 className="text-lg font-bold">{task.name}</h1>
       <p className="">{task.points} Points Upon Completion</p>
     </div>
--- src/components/universal/taskSelector.tsx
+++ src/components/universal/taskSelector.tsx
@@ -2,43 +2,59 @@ import React, { useState } from "react";
 import { api } from "~/utils/api";
 import { type ClubNamesType } from "../types/clubs";
 import TaskCard from "./TaskCard";
+import UserCompletedTask from "./UserCompletedTasks";
+import { type Task } from "@prisma/client";
 
-const TaskSelector = ({societyId, setTaskId}: {societyId: ClubNamesType, setTaskId: CallableFunction}) => {
 
+const TaskSelector = (
+  { societyId, setTask, userId }:
+    {
+      societyId: ClubNamesType,
+      setTask: CallableFunction,
+      userId: string,
+    }) => {
 
   // present a list of tasks in the society.
-
   const [selectedTask, setSelectedTask] = useState('');
 
-  const { data: taskList } = api.quests.getSocietyTasks.useQuery({
-    societyNames: [societyId]
-  })
+  const { data: taskList } = api.quests.getUserSocietyTasks.useQuery({
+    societyName: societyId,
+    userId: userId
+  });
+  const handleClick = (task: Task) => {
 
-  const handleClick = (taskId: string) => {
-    if (selectedTask === taskId) {
-      setTaskId("");
+    if (selectedTask === task.id) {
+      setTask(null);
       setSelectedTask("");
-
     } else {
-      setTaskId(taskId);
-      setSelectedTask(taskId);
-    } 
-  }
-  return (
-  <div className=" w-full flex flex-row overflow-y-scroll items-center border-2 border-black">
-    {taskList?.tasks.map((task: {id: string, name: string, points: number, societyId: string | null;}, index) => {
-      return (
-      <div key={index} className=" p-1 m-2">
-        <div className={`  ${selectedTask === task.id ? 'border-4 border-green-500': ''}`} >
-        <button onClick={() => {handleClick(task.id)}}>
-          <TaskCard task={task}/>
-        </button>
-        </div>
-      </div>
-      );
-    })}
+      setTask(task);
+      setSelectedTask(task.id);
+    }}
 
-  </div>
+  return (
+    <div className="mb-2 mt-2">
+      <div className="w-full items-center text-center text-xl text-white">Select Task</div>
+      <div className=" w-full flex flex-row overflow-y-scroll items-center  ">
+        {taskList?.incompleteTasks.map((task: Task, index) => {
+          return (
+            <div key={index} className=" p-1 m-2">
+              <div>
+                  
+                <button onClick={() => { handleClick(task) }}>
+                {selectedTask === task.id ? 
+                  <TaskCard task={task} selected={true}/> : <TaskCard task={task} selected={false}/>}
+                </button> 
+              </div>
+            </div>
+          );
+        })}
+
+      </div>
+      <div className="mb-2"></div>
+      <div className="w-full items-center text-center text-xl text-white">Completed Tasks</div>
+      <UserCompletedTask completedTasks={taskList?.completedTasks} />
+
+    </div>
   );
 
 
